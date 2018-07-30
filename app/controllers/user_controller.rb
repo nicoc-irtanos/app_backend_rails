9ae1495939a32639434181db09b36836c788a6b7
Dyf-Tfh <n.zhao@hotmail.fr>
2018-07-30 15:31:11 +0200

    error

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,11 +3,19 @@ class UserController < ApplicationController
   end
 
   def create
-    user = User.create!(username: params[:username], bio: params[:bio])
-    redirect_to("/user/#{user.id}")
+    user = User.create(username: params[:username], bio: params[:bio])
+    puts user.id.class
+    if user.id == nil
+      redirect_to("/user/error")
+    else
+      redirect_to("/user/#{user.id}")
+    end
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def error
   end
 end
