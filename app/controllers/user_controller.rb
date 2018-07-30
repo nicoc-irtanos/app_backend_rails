b921ccb01da8a7b20e4cdc7e60efaaa6f8b279fa
nicolasculerier <nicolas.culerier@laposte.net>
2018-07-30 16:14:03 +0200

    route via username marche

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,15 +5,15 @@ class UserController < ApplicationController
   def create
     user = User.create(username: params[:username], bio: params[:bio])
     puts user.id.class
-    if user.id == nil
+    if user.username == nil
       redirect_to("/user/error")
     else
-      redirect_to("/user/#{user.id}")
+      redirect_to("/user/#{user.username}")
     end
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by_username(params[:id])
   end
 
   def error
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   validates :username, uniqueness: true
   validates :username, format: { without: /\s/ }
+
+  def to_param
+    username
+  end
 end
